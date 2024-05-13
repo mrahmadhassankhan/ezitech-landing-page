3d43a67f0986e4947d8738f6ee74f94c23122ce1
AhmadHassanKhan <fiverrahmadhassankhan@gmail.com>
2024-05-13 23:11:00 +0500

    Fully Responsive Done

--- src/components/Header-Default.jsx
+++ src/components/Header-Default.jsx
@@ -2,13 +2,24 @@ import "../css/Header-Default.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 const HeaderDefault = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <>
       <div className="header-container">
         <div className="logo-website">
           <img src="/static/Images/EZITECH-LOGO-white.png" alt="logo" />
-          <img src="/static/Images/EZITECH-LOGO-.jpg" alt="" />
         </div>
         <div className="header-menu">
           <ul>
@@ -31,9 +42,6 @@ const HeaderDefault = () => {
               <a href="http://ezitech.org/iportal/">INTERN PORTAL</a>
             </li>
           </ul>
-          <span>
-            <FontAwesomeIcon icon={faBars} color="#2575ed"></FontAwesomeIcon>
-          </span>
         </div>
 
         <div className="search-with-heart">
@@ -51,6 +59,54 @@ const HeaderDefault = () => {
             </li>
           </ul>
         </div>
+      </div>
+
+      <div className="navbar-expanded">
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <img
+              src="/static/Images/EZITECH-LOGO-.jpg"
+              alt=""
+              width={170}
+              height={60}
+            />
+            <Navbar.Brand href="#"></Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "300px" }}
+                navbarScroll
+              >
+                <Nav.Link href="https://ezitech.org/">HOME</Nav.Link>
+                <Nav.Link href="https://ezitech.org/about-ezitech/">
+                  ABOUT US
+                </Nav.Link>
+                <Nav.Link href="https://ezitech.org/certifications/">
+                  CERTIFICATIONS
+                </Nav.Link>
+                <Nav.Link href="http://ezitech.org/internship-program/">
+                  INTERNSHIPS
+                </Nav.Link>
+                <Nav.Link href="https://ezitech.org/seminars/">
+                  SEMINARS
+                </Nav.Link>
+                <Nav.Link href="http://ezitech.org/iportal/">
+                  INTERN PORTAL
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
     </>
   );
